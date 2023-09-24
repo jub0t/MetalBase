69caad23b3e17a4047a7bfc516dd75e1bd89f3ee
Jareer Abdullah <jforeverything2007@gmail.com>
2023-09-24 22:26:49 +0200

    Update image.rs

--- src/database/image.rs
+++ src/database/image.rs
@@ -1,2 +1,14 @@
 #[derive(PartialEq, Serialize, Deserialize, Clone, Debug)]
-pub struct ImagePointer {}
+pub struct ImagePointer {
+    image_id: String,
+}
+
+impl ImagePointer {
+    pub fn new(image_id: String) -> Self {
+        Self { image_id }
+    }
+
+    pub fn set_id(&mut self, image_id: String) {
+        self.image_id = image_id
+    }
+}
